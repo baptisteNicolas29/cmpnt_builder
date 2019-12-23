a045b5e078f3974d20313292717077d150e3a9cd
Baptiste Nicolas <baptiste.nicolas.3dwp@gmail.com>
2019-12-23 14:55:36 +0100

    delete function working for create_cmpnt_function

--- create_cmpnt.py
+++ create_cmpnt.py
@@ -1,42 +1,37 @@
 from maya import cmds
 
-class CreateCmpnt:
+def set_outliner_color(self, node, outliner_color= [1, 1, 1]):
 
-    def __init__(self):
-        pass
+    cmds.setAttr(node+ '.useOutlinerColor', 1)
+    cmds.setAttr(node+ '.outlinerColorR', outliner_color[0])
+    cmds.setAttr(node+ '.outlinerColorG', outliner_color[1])
+    cmds.setAttr(node+ '.outlinerColorB', outliner_color[2])
 
-    def set_outliner_color(self, node, outliner_color= [1, 1, 1]):
+def create_multiple_cmpnt(self, cmpnt_list= cmds.ls(sl= 1)):
 
-        cmds.setAttr(node+ '.useOutlinerColor', 1)
-        cmds.setAttr(node+ '.outlinerColorR', outliner_color[0])
-        cmds.setAttr(node+ '.outlinerColorG', outliner_color[1])
-        cmds.setAttr(node+ '.outlinerColorB', outliner_color[2])
+    for node in cmpnt_list:
 
-    def create_multiple_cmpnt(self, cmpnt_list= cmds.ls(sl= 1)):
+        self.create_cmpnt(node)
 
-        for node in cmpnt_list:
+def create_cmpnt(self, cmpnt_name):
 
-            self.create_cmpnt(node)
+    parent_node= None
 
-    def create_cmpnt(self, cmpnt_name):
+    if cmds.objExists(cmpnt_name):
 
-        parent_node= None
+        cmds.delete(cmpnt_name)
 
-        if cmds.objExists(cmpnt_name):
+    cmds.createNode('transform', n= '{0}:_cmpnt'.format(cmpnt_name), p= '__ctrl_grp' if cmds.objExists('__ctrl_grp') else None, ss= 1)
+    cmds.createNode('transform', n= '{0}:_input'.format(cmpnt_name), p= '{0}:_cmpnt'.format(cmpnt_name), ss= 1)
+    cmds.createNode('transform', n= '{0}:_output'.format(cmpnt_name), p= '{0}:_cmpnt'.format(cmpnt_name), ss= 1)
+    cmds.createNode('transform', n= '{0}:_public'.format(cmpnt_name), p= '{0}:_cmpnt'.format(cmpnt_name), ss= 1)
+    cmds.createNode('transform', n= '{0}:_private'.format(cmpnt_name), p= '{0}:_cmpnt'.format(cmpnt_name), ss= 1)
+    cmds.createNode('transform', n= '{0}:_deform'.format(cmpnt_name), p= '{0}:_private'.format(cmpnt_name), ss= 1)
+    cmds.createNode('transform', n= '{0}:_proxi'.format(cmpnt_name), p= '{0}:_private'.format(cmpnt_name), ss= 1)
 
-            cmds.delete(cmpnt_name)
-
-        cmds.createNode('transform', n= '{0}:_cmpnt'.format(cmpnt_name), p= '__ctrl_grp' if cmds.objExists('__ctrl_grp') else None, ss= 1)
-        cmds.createNode('transform', n= '{0}:_input'.format(cmpnt_name), p= '{0}:_cmpnt'.format(cmpnt_name), ss= 1)
-        cmds.createNode('transform', n= '{0}:_output'.format(cmpnt_name), p= '{0}:_cmpnt'.format(cmpnt_name), ss= 1)
-        cmds.createNode('transform', n= '{0}:_public'.format(cmpnt_name), p= '{0}:_cmpnt'.format(cmpnt_name), ss= 1)
-        cmds.createNode('transform', n= '{0}:_private'.format(cmpnt_name), p= '{0}:_cmpnt'.format(cmpnt_name), ss= 1)
-        cmds.createNode('transform', n= '{0}:_deform'.format(cmpnt_name), p= '{0}:_private'.format(cmpnt_name), ss= 1)
-        cmds.createNode('transform', n= '{0}:_proxi'.format(cmpnt_name), p= '{0}:_private'.format(cmpnt_name), ss= 1)
-
-        self.set_outliner_color('{0}:_input'.format(cmpnt_name), [.5, .5, 1])
-        self.set_outliner_color('{0}:_output'.format(cmpnt_name), [.5, .5, 1])
-        self.set_outliner_color('{0}:_public'.format(cmpnt_name), [.5, 1, .5])
-        self.set_outliner_color('{0}:_private'.format(cmpnt_name), [1, .5, .5])
-        self.set_outliner_color('{0}:_deform'.format(cmpnt_name), [1, 1, .5])
-        self.set_outliner_color('{0}:_proxi'.format(cmpnt_name), [1, 1, .5])
+    self.set_outliner_color('{0}:_input'.format(cmpnt_name), [.5, .5, 1])
+    self.set_outliner_color('{0}:_output'.format(cmpnt_name), [.5, .5, 1])
+    self.set_outliner_color('{0}:_public'.format(cmpnt_name), [.5, 1, .5])
+    self.set_outliner_color('{0}:_private'.format(cmpnt_name), [1, .5, .5])
+    self.set_outliner_color('{0}:_deform'.format(cmpnt_name), [1, 1, .5])
+    self.set_outliner_color('{0}:_proxi'.format(cmpnt_name), [1, 1, .5])
