d5a0426f74d763a69aac9899e5e0e921f15bb7ad
baptiste nicolas <baptiste.nicolas.3dwp@gmail.com>
2019-12-23 18:22:03 +0100

    fixing doubling component

--- create_cmpnt.py
+++ create_cmpnt.py
@@ -21,13 +21,13 @@ def create_cmpnt(cmpnt_name):
 
         cmds.delete(cmpnt_name)
 
-    cmds.createNode('transform', n= '{0}:_cmpnt'.format(cmpnt_name), p= '__ctrl_grp' if cmds.objExists('__ctrl_grp') else None, ss= 1)
-    cmds.createNode('transform', n= '{0}:_input'.format(cmpnt_name), p= '{0}:_cmpnt'.format(cmpnt_name), ss= 1)
-    cmds.createNode('transform', n= '{0}:_output'.format(cmpnt_name), p= '{0}:_cmpnt'.format(cmpnt_name), ss= 1)
-    cmds.createNode('transform', n= '{0}:_public'.format(cmpnt_name), p= '{0}:_cmpnt'.format(cmpnt_name), ss= 1)
-    cmds.createNode('transform', n= '{0}:_private'.format(cmpnt_name), p= '{0}:_cmpnt'.format(cmpnt_name), ss= 1)
-    cmds.createNode('transform', n= '{0}:_deform'.format(cmpnt_name), p= '{0}:_private'.format(cmpnt_name), ss= 1)
-    cmds.createNode('transform', n= '{0}:_proxi'.format(cmpnt_name), p= '{0}:_private'.format(cmpnt_name), ss= 1)
+    cmds.createNode('transform', n= '{0}:_cmpnt'.format(cmpnt_name), p= '__ctrl_grp' if cmds.objExists('__ctrl_grp') else None, ss= 1) if not cmds.objExists('{0}:_cmpnt'.format(cmpnt_name)) else None
+    cmds.createNode('transform', n= '{0}:_input'.format(cmpnt_name), p= '{0}:_cmpnt'.format(cmpnt_name), ss= 1) if not cmds.objExists('{0}:_input'.format(cmpnt_name)) else None
+    cmds.createNode('transform', n= '{0}:_output'.format(cmpnt_name), p= '{0}:_cmpnt'.format(cmpnt_name), ss= 1) if not cmds.objExists('{0}:_output'.format(cmpnt_name)) else None
+    cmds.createNode('transform', n= '{0}:_public'.format(cmpnt_name), p= '{0}:_cmpnt'.format(cmpnt_name), ss= 1) if not cmds.objExists('{0}:_public'.format(cmpnt_name)) else None
+    cmds.createNode('transform', n= '{0}:_private'.format(cmpnt_name), p= '{0}:_cmpnt'.format(cmpnt_name), ss= 1) if not cmds.objExists('{0}:_private'.format(cmpnt_name)) else None
+    cmds.createNode('transform', n= '{0}:_deform'.format(cmpnt_name), p= '{0}:_private'.format(cmpnt_name), ss= 1) if not cmds.objExists('{0}:_deform'.format(cmpnt_name)) else None
+    cmds.createNode('transform', n= '{0}:_proxi'.format(cmpnt_name), p= '{0}:_private'.format(cmpnt_name), ss= 1) if not cmds.objExists('{0}:_proxi'.format(cmpnt_name)) else None
 
     set_outliner_color('{0}:_input'.format(cmpnt_name), [.5, .5, 1])
     set_outliner_color('{0}:_output'.format(cmpnt_name), [.5, .5, 1])
