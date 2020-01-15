ad77a0802bc2596f15061270e0f2c8a09b72c70f
baptiste <baptiste.nicolas.protonmail.com>
2020-01-15 14:43:43 +0100

    connect_ui_to functions

--- cmpnt_builder_ui.py
+++ cmpnt_builder_ui.py
@@ -1,16 +1,25 @@
 from PySide2 import QtCore, QtGui, QtWidgets
+import connections_setup
+import create_cmpnt
+import rig_initialiser
+
+reload(rig_initialiser)
+reload(create_cmpnt)
+reload(connections_setup)
 
 class Cmpnt_builder(QtWidgets.QWidget):
 
     def __init__(self, parent= None):
 
         super(Cmpnt_builder, self).__init__(parent)
+        self.rig_initialiser= rig_initialiser.Rig_Initialiser()
 
         self.addItem()
         self.addLayout()
         self.addConnection()
 
     def addItem(self):
+
         self.menu_bar= QtWidgets.QMenuBar()
         self.file_menu= self.menu_bar.addMenu('File')
         self.help_menu= self.menu_bar.addMenu('Help')
@@ -29,14 +38,14 @@ class Cmpnt_builder(QtWidgets.QWidget):
         self.help_menu.addAction(self.about_action)
         self.help_menu.addAction(self.api_action)
 
-        self.list_btn= [
-        QtWidgets.QPushButton('Create\nCmpnt'),
-        QtWidgets.QPushButton('Output\nTo\nInput'),
-        QtWidgets.QPushButton('Set\nInput'),
-        QtWidgets.QPushButton('Set\nOutput'),
-        QtWidgets.QPushButton('Spawn\nControler'),
-        QtWidgets.QPushButton('Create\nBuffer')
-        ]
+        self.list_btn= {
+        'build_cmpnt': QtWidgets.QPushButton('Create\nCmpnt'),
+        'output_to_input': QtWidgets.QPushButton('Output\nTo\nInput'),
+        'set_input': QtWidgets.QPushButton('Set\nInput'),
+        'set_output': QtWidgets.QPushButton('Set\nOutput'),
+        'buid_ctrl': QtWidgets.QPushButton('Spawn\nControler'),
+        'build_buffer': QtWidgets.QPushButton('Create\nBuffer')
+        }
 
         self.iconList= [
         '/home/baptiste_nicolas/maya/2018/prefs/icons/cmpnt.svg',
@@ -46,12 +55,13 @@ class Cmpnt_builder(QtWidgets.QWidget):
         '/home/baptiste_nicolas/maya/2018/prefs/icons/buffer.svg'
         ]
 
-        for idx, btn in enumerate(self.list_btn):
+        for idx, btn_id in enumerate(self.list_btn):
+
+            btn= self.list_btn[btn_id]
 
             #btn.setIcon(QtGui.QIcon(self.iconList[idx]))
-            #btn.setIconSize(QtCore.QSize(50,50))
-            #btn.setLayoutDirection(QtCore.Qt.RightToLeft)
-
+            btn.setIconSize(QtCore.QSize(50,50))
+            btn.setLayoutDirection(QtCore.Qt.RightToLeft)
 
             btn.setMaximumHeight(75)
             btn.setMaximumWidth(75)
@@ -60,17 +70,32 @@ class Cmpnt_builder(QtWidgets.QWidget):
             btn.setMinimumWidth(75)
 
     def addLayout(self):
+
         self.mainLayout= QtWidgets.QVBoxLayout()
         self.btnLayout= QtWidgets.QHBoxLayout()
 
-
         self.btnLayout.setMenuBar(self.menu_bar)
-        for btn in self.list_btn:
-            self.btnLayout.addWidget(btn)
 
+
+        self.btnLayout.addWidget(self.list_btn['build_cmpnt'])
+        self.btnLayout.addWidget(self.list_btn['output_to_input'])
+        self.btnLayout.addWidget(self.list_btn['set_input'])
+        self.btnLayout.addWidget(self.list_btn['set_output'])
+        self.btnLayout.addWidget(self.list_btn['buid_ctrl'])
+        self.btnLayout.addWidget(self.list_btn['build_buffer'])
 
         self.setLayout(self.btnLayout)
 
     def addConnection(self):
 
         self.exit_action.triggered.connect(self.close)
+        self.init_action.triggered.connect(self.rig_initialiser.build_base_rig)
+        self.finalise_action.triggered.connect(self.rig_initialiser.rig_finaliser)
+
+        self.list_btn['build_cmpnt'].clicked.connect(create_cmpnt.create_multiple_cmpnt)
+
+        self.list_btn['output_to_input'].clicked.connect(connections_setup.output_to_input)
+        self.list_btn['set_input'].clicked.connect(connections_setup.ctrl_to_input)
+        self.list_btn['set_output'].clicked.connect(connections_setup.ctrl_to_output)
+
+        self.list_btn['build_buffer'].clicked.connect(create_cmpnt.build_buffer)
